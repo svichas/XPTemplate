9141c774df7952d448b06f4eadd371ada92bf129
Stefanos Vichas <stefanosvchs@gmail.com>
2018-12-14 21:28:02 +0200

    Create tasknar

---
+++ jsbin/core.js
@@ -0,0 +1,12 @@
+
+
+function toggleStartMenu() {
+  var startMenu = document.querySelector(".xp_startmenu")
+  startMenu.classList.toggle("open");
+}
+
+
+function closeStartMenu() {
+  var startMenu = document.querySelector(".xp_startmenu")
+  startMenu.classList.remove("open");
+}
