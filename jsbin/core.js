f7721a3ead42d0cadd56aa761af64fd576fb42b0
Stefanos Vichas <stefanosvchs@gmail.com>
2018-12-14 21:47:58 +0200

    Add xp bottom bar clock

--- jsbin/core.js
+++ jsbin/core.js
@@ -10,3 +10,18 @@ function closeStartMenu() {
   var startMenu = document.querySelector(".xp_startmenu")
   startMenu.classList.remove("open");
 }
+
+
+
+
+function setTime() {
+  var timeElement = document.querySelector(".xp_bottom_bar_time");
+  var currentdate = new Date();
+  timeElement.innerHTML = currentdate.getHours() + ":" + currentdate.getMinutes();
+}
+
+
+window.onload = function() {
+  setTime();
+  setInterval(setTime, 1000);
+}
