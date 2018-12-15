4a50429656a8eea44ddbe4b178862ad03c59ddaa
Stefanos Vichas <stefanosvchs@gmail.com>
2018-12-15 12:07:38 +0200

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ const gulp   = require('gulp');
 const pump   = require('pump');
 const rename = require("gulp-rename");
 const sass   = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('compress-sass', function (cb) {
   pump([
         gulp.src('cssbin/scss/*.scss'),
         sass({outputStyle: "compressed"}).on("error", sass.logError),
         rename("core.min.css"),
+        autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }),
         gulp.dest('cssbin'),
     ],
     cb
